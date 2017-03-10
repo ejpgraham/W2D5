42de6c80dccea72388860532766ad1c65f0e5fe5
Edward Graham <ejpgraham@gmail.com>
2017-03-10 12:24:52 -0500

    Completed Phase II

--- skeleton/lib/p02_hashing.rb
+++ skeleton/lib/p02_hashing.rb
@@ -4,11 +4,25 @@ end
 
 class Array
   def hash
+    sum = self.first
+    self.each_with_index do |el, idx|
+      next if idx == 0
+      sum += el * (idx + 1)
+    end
+    sum.hash
   end
 end
 
 class String
   def hash
+    alphabet = ("A".."Z").to_a + ("a".."z").to_a
+
+    sum = 0
+    self.chars.each_with_index do |char, idx|
+      sum += alphabet.index(char) * (idx + 1)
+    end
+
+    sum.hash
   end
 end
 
@@ -16,6 +30,16 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    alphabet = ("A".."Z").to_a + ("a".."z").to_a
+
+    sum = 0
+    self.each do |key, val|
+      if val.is_a?(Integer)
+        sum += alphabet.index(key.to_s) * val
+      else
+        sum += alphabet.index(key.to_s) * alphabet.index(val.to_s)
+      end
+    end
+    sum.hash
   end
 end
